bfb6817bb8db19288d9d06702dd85dd4181c2042
pedrogbraz <pedrogbraz@users.noreply.github.com>
2026-06-21 18:20:23 -0300

    feat(ui): add Table primitives

---
+++ packages/ui/src/components/table.tsx
@@ -0,0 +1,131 @@
+import {
+  forwardRef,
+  type HTMLAttributes,
+  type TdHTMLAttributes,
+  type ThHTMLAttributes,
+} from "react";
+import { cn } from "../lib/cn.js";
+
+export const Table = forwardRef<HTMLTableElement, HTMLAttributes<HTMLTableElement>>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div data-slot="table-container" className="relative w-full overflow-x-auto">
+        <table
+          ref={ref}
+          data-slot="table"
+          className={cn("w-full caption-bottom text-sm text-fg", className)}
+          {...props}
+        />
+      </div>
+    );
+  },
+);
+Table.displayName = "Table";
+
+export const TableHeader = forwardRef<
+  HTMLTableSectionElement,
+  HTMLAttributes<HTMLTableSectionElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <thead
+      ref={ref}
+      data-slot="table-header"
+      className={cn("[&_tr]:border-b [&_tr]:border-border", className)}
+      {...props}
+    />
+  );
+});
+TableHeader.displayName = "TableHeader";
+
+export const TableBody = forwardRef<
+  HTMLTableSectionElement,
+  HTMLAttributes<HTMLTableSectionElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <tbody
+      ref={ref}
+      data-slot="table-body"
+      className={cn("[&_tr:last-child]:border-0", className)}
+      {...props}
+    />
+  );
+});
+TableBody.displayName = "TableBody";
+
+export const TableFooter = forwardRef<
+  HTMLTableSectionElement,
+  HTMLAttributes<HTMLTableSectionElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <tfoot
+      ref={ref}
+      data-slot="table-footer"
+      className={cn("border-t border-border bg-surface-inset/50 font-medium", className)}
+      {...props}
+    />
+  );
+});
+TableFooter.displayName = "TableFooter";
+
+export const TableRow = forwardRef<HTMLTableRowElement, HTMLAttributes<HTMLTableRowElement>>(
+  ({ className, ...props }, ref) => {
+    return (
+      <tr
+        ref={ref}
+        data-slot="table-row"
+        className={cn(
+          "border-b border-border transition-colors hover:bg-surface-overlay/50 data-[state=selected]:bg-surface-overlay",
+          className,
+        )}
+        {...props}
+      />
+    );
+  },
+);
+TableRow.displayName = "TableRow";
+
+export const TableHead = forwardRef<HTMLTableCellElement, ThHTMLAttributes<HTMLTableCellElement>>(
+  ({ className, ...props }, ref) => {
+    return (
+      <th
+        ref={ref}
+        data-slot="table-head"
+        className={cn(
+          "h-10 px-3 text-left align-middle font-medium text-fg-secondary whitespace-nowrap",
+          className,
+        )}
+        {...props}
+      />
+    );
+  },
+);
+TableHead.displayName = "TableHead";
+
+export const TableCell = forwardRef<HTMLTableCellElement, TdHTMLAttributes<HTMLTableCellElement>>(
+  ({ className, ...props }, ref) => {
+    return (
+      <td
+        ref={ref}
+        data-slot="table-cell"
+        className={cn("p-3 align-middle whitespace-nowrap", className)}
+        {...props}
+      />
+    );
+  },
+);
+TableCell.displayName = "TableCell";
+
+export const TableCaption = forwardRef<
+  HTMLTableCaptionElement,
+  HTMLAttributes<HTMLTableCaptionElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <caption
+      ref={ref}
+      data-slot="table-caption"
+      className={cn("mt-4 text-sm text-fg-tertiary", className)}
+      {...props}
+    />
+  );
+});
+TableCaption.displayName = "TableCaption";
