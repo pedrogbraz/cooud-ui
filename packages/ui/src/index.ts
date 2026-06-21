9bbac6b9bf432063f11e96e6e88017bda205f774
pedrogbraz <pedrogbraz@users.noreply.github.com>
2026-06-21 16:32:29 -0300

    feat(ui): add barrel exports

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,25 @@
+// Utilities
+
+export type { BadgeProps } from "./components/badge.js";
+
+// Components (Wave 0)
+export { Badge, badgeVariants } from "./components/badge.js";
+export type { ButtonProps } from "./components/button.js";
+export { Button, buttonVariants } from "./components/button.js";
+export {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/card.js";
+export type { InputProps } from "./components/input.js";
+export { Input } from "./components/input.js";
+export { Label } from "./components/label.js";
+export { Separator } from "./components/separator.js";
+export { Skeleton } from "./components/skeleton.js";
+export type { SpinnerProps } from "./components/spinner.js";
+export { Spinner } from "./components/spinner.js";
+export { cn } from "./lib/cn.js";
