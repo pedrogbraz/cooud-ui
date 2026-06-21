70028daf6976ff3f86897ff5833a14da7f67c825
pedrogbraz <pedrogbraz@users.noreply.github.com>
2026-06-21 16:32:18 -0300

    feat(theme): expose package exports

---
+++ packages/theme/src/index.ts
@@ -0,0 +1,4 @@
+export type { CooudUIProviderProps } from "./provider.js";
+export { CooudUIProvider } from "./provider.js";
+export type { ThemeContextValue } from "./use-theme.js";
+export { useTheme } from "./use-theme.js";
